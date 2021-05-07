5f95187907dc213c29e7231b501b333119f604d5
Alessandra Palazzo <apalazzo@wn-10.le.infn.it>
2021-05-07 14:01:22 +0200

    Improvements to the PDNN

--- buildPDNN.py
+++ buildPDNN.py
@@ -2,7 +2,7 @@
 from Functions import *
 
 plot = True
-NN = 'pDNN'
+NN = 'PDNN'
 
 ### Reading the command line
 analysis, channel, numberOfNodes, numberOfLayers, numberOfEpochs, validationFraction, dropout, trainingFraction = ReadArgParser()
@@ -26,26 +26,9 @@ logString = 'Input dataframe: ' + dfInput + '\nNumber of input events: ' + str(X
 logFile.write(logString)
 logInfo += logString
 
-### Splitting into test/train samples                                                                                                                        
-Ntrain_stop = int(round(X_input.shape[0] * trainingFraction))
-X_train = X_input[:Ntrain_stop]
-X_test = X_input[Ntrain_stop:]
-y_train = y_input[:Ntrain_stop]
-y_test = y_input[Ntrain_stop:]
-
-### Dividing signal from background
-X_test_signal_unscaled = X_test[y_test == 1]
-X_test_bkg_unscaled = X_test[y_test != 1]
-X_train_signal_unscaled = X_train[y_train == 1]
-X_train_bkg_unscaled = X_train[y_train != 1]
-
-logString = '\nNumber of train events: ' + str(X_train.shape[0]) + ' (' + str(X_train_signal_unscaled.shape[0]) + ' signal and ' + str(X_train_bkg_unscaled.shape[0]) + ' background)' + '\nNumber of test events: ' + str(X_test.shape[0]) + ' (' + str(X_test_signal_unscaled.shape[0]) + ' signal and ' + str(X_test_bkg_unscaled.shape[0]) + ' background)'
-logFile.write(logString) 
-logInfo = logInfo + logString
-
 ### Saving unscaled test mass values, sorted in ascending order
 unscaledMass = []
-for event in X_test_signal_unscaled:
+for event in X_input:
     unscaledMass.append(event[-1])
 unscaledMassPointsList = sorted(list(set(unscaledMass)))
 print('Mass points: ' + str(unscaledMassPointsList))
@@ -53,67 +36,75 @@ logString = '\nMass points: ' + str(unscaledMassPointsList)
 logFile.write(logString)
 logInfo = logInfo + logString
 
-### Weighting train events
-w_train_signal, w_train_bkg = EventsWeight(X_train_signal_unscaled, X_train_bkg_unscaled)
-logString = '\nWeight for signal train events: ' + str(w_train_signal) + '\nWeight for background train events: ' + str(w_train_bkg)
-logFile.write(logString)
-logInfo += logString
-
-### Creating new extended train arrays by adding W_train (this information is needed in order to shuffle data properly)
-X_train_signal_ext = np.insert(X_train_signal_unscaled, X_train_signal_unscaled.shape[1], w_train_signal, axis = 1)
-X_train_bkg_ext = np.insert(X_train_bkg_unscaled, X_train_bkg_unscaled.shape[1], w_train_bkg, axis = 1)
-
-### Creating new extended train/test arrays by adding value 1 (0) to signal (background) events (this information is needed in order to shuffle data properly)    
-X_train_signal_ext = np.insert(X_train_signal_ext, X_train_signal_ext.shape[1], 1, axis = 1)
-X_train_bkg_ext = np.insert(X_train_bkg_ext, X_train_bkg_ext.shape[1], 0, axis = 1)
-X_test_signal_ext = np.insert(X_test_signal_unscaled, X_test_signal_unscaled.shape[1], 1, axis = 1)
-X_test_bkg_ext = np.insert(X_test_bkg_unscaled, X_test_bkg_unscaled.shape[1], 0, axis = 1)
-
-### Putting train/test signal and background events back together
-X_train_ext_unscaled = np.concatenate((X_train_signal_ext, X_train_bkg_ext), axis = 0)
-X_test_ext_unscaled = np.concatenate((X_test_signal_ext, X_test_bkg_ext), axis = 0)
-
-### Shuffling train/test data
-X_train_ext_unscaled = ShufflingData(X_train_ext_unscaled)
-X_test_ext_unscaled = ShufflingData(X_test_ext_unscaled)
-
-### Extracting y_train/y_test from X_train_ext/X_test_ext
-y_train = X_train_ext_unscaled[:, X_train_ext_unscaled.shape[1] - 1]
-y_test = X_test_ext_unscaled[:, X_test_ext_unscaled.shape[1] - 1]
-
-### Deleting y_train/y_test from X_train_ext/X_test_ext
-X_train_ext_unscaled = np.delete(X_train_ext_unscaled, X_train_ext_unscaled.shape[1] - 1, axis = 1)
-X_test_unscaled = np.delete(X_test_ext_unscaled, X_test_ext_unscaled.shape[1] - 1, axis = 1)
-
-### Extracting w_train from X_train_ext
-w_train = X_train_ext_unscaled[:, X_train_ext_unscaled.shape[1] - 1]
-
-### Deleting w_train from X_train_ext
-X_train_unscaled = np.delete(X_train_ext_unscaled, X_train_ext_unscaled.shape[1] - 1, axis = 1)
-
 ### Scaling train/test data
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train_unscaled)
-X_test = scaler.fit_transform(X_test_unscaled)
+X_scaled = scaler.fit_transform(X_input)
 
-### Extracting scaled signal and background events
+### Splitting into test/train samples                                                                                                                        
+Ntrain_stop = int(round(X_scaled.shape[0] * trainingFraction))
+X_train = X_scaled[:Ntrain_stop]
+X_test = X_scaled[Ntrain_stop:]
+y_train = y_input[:Ntrain_stop]
+y_test = y_input[Ntrain_stop:]
+
+### Dividing signal from background
 X_test_signal = X_test[y_test == 1]
 X_test_bkg = X_test[y_test != 1]
 X_train_signal = X_train[y_train == 1]
 X_train_bkg = X_train[y_train != 1]
 
-### Extracting scaled signal train/test masses
+logString = '\nNumber of train events: ' + str(X_train.shape[0]) + ' (' + str(X_train_signal.shape[0]) + ' signal and ' + str(X_train_bkg.shape[0]) + ' background)' + '\nNumber of test events: ' + str(X_test.shape[0]) + ' (' + str(X_test_signal.shape[0]) + ' signal and ' + str(X_test_bkg.shape[0]) + ' background)'
+logFile.write(logString) 
+logInfo = logInfo + logString
+
+### Extracting scaled signal test masses
 m_test_signal = X_test_signal[:, X_test_signal.shape[1] - 1]
-m_train_signal = X_train_signal[:, X_train_signal.shape[1] - 1]
 
 ### Saving scaled signal train/test massed, sorted in ascending order (the same unscaled train/test mass values are scaled into different train/test mass values. If we sort them in ascending order we should keep the correspondence between scaled and unscaled values)
 scaledTestMassPointsList = sorted(list(set(m_test_signal)))
-scaledTrainMassPointsList = sorted(list(set(m_train_signal)))
 
-### Building the pDNN
+### Weighting train events
+w_train_signal, w_train_bkg = EventsWeight(X_train_signal, X_train_bkg)
+logString = '\nWeight for signal train events: ' + str(w_train_signal) + '\nWeight for background train events: ' + str(w_train_bkg)
+logFile.write(logString)
+logInfo += logString
+
+### Creating new extended train arrays by adding W_train (this information is needed in order to shuffle data properly)
+X_train_signal_ext = np.insert(X_train_signal, X_train_signal.shape[1], w_train_signal, axis = 1)
+X_train_bkg_ext = np.insert(X_train_bkg, X_train_bkg.shape[1], w_train_bkg, axis = 1)
+
+### Creating new extended train/test arrays by adding value 1 (0) to signal (background) events (this information is needed in order to shuffle data properly)    
+X_train_signal_ext = np.insert(X_train_signal_ext, X_train_signal_ext.shape[1], 1, axis = 1)
+X_train_bkg_ext = np.insert(X_train_bkg_ext, X_train_bkg_ext.shape[1], 0, axis = 1)
+X_test_signal_ext = np.insert(X_test_signal, X_test_signal.shape[1], 1, axis = 1)
+X_test_bkg_ext = np.insert(X_test_bkg, X_test_bkg.shape[1], 0, axis = 1)
+
+### Putting train/test signal and background events back together
+X_train_ext = np.concatenate((X_train_signal_ext, X_train_bkg_ext), axis = 0)
+X_test_ext = np.concatenate((X_test_signal_ext, X_test_bkg_ext), axis = 0)
+
+### Shuffling train/test data
+X_train_ext = ShufflingData(X_train_ext)
+X_test_ext = ShufflingData(X_test_ext)
+
+### Extracting y_train/y_test from X_train_ext/X_test_ext
+y_train = X_train_ext[:, X_train_ext.shape[1] - 1]
+y_test = X_test_ext[:, X_test_ext.shape[1] - 1]
+
+### Deleting y_train/y_test from X_train_ext/X_test_ext
+X_train_ext = np.delete(X_train_ext, X_train_ext.shape[1] - 1, axis = 1)
+X_test = np.delete(X_test_ext, X_test_ext.shape[1] - 1, axis = 1)
+
+### Extracting w_train from X_train_ext
+w_train = X_train_ext[:, X_train_ext.shape[1] - 1]
+
+### Deleting w_train from X_train_ext
+X_train = np.delete(X_train_ext, X_train_ext.shape[1] - 1, axis = 1)
+
+### Building the PDNN
 n_dim = X_train.shape[1]
 
 model = BuildDNN(n_dim, numberOfNodes, numberOfLayers, dropout)
@@ -122,12 +113,15 @@ model.compile(loss = 'binary_crossentropy', optimizer = 'rmsprop', metrics = ['a
 ### Training
 callbacks = [
     # If we don't have a decrease of the loss for 11 epochs, terminate training.
-    EarlyStopping(verbose = True, patience = 10, monitor = 'val_loss')
+    EarlyStopping(verbose = True, patience = 10, monitor = 'val_loss')#, ModelCheckpoint(outputDir+'/model.h5', monitor='val_loss', verbose=True, save_best_only=True)
 ]
+
+#del model
+#model = load_model(outputDir + '/model.h5')
 
 modelMetricsHistory = model.fit(X_train, y_train, sample_weight = w_train, epochs = numberOfEpochs, batch_size = 2048, validation_split = validationFraction, verbose = 1, callbacks = callbacks)
 
-### Evaluating the performance of the pDNN
+### Evaluating the performance of the PDNN
 testLoss, testAccuracy = EvaluatePerformance(model, X_test, y_test)
 
 logString = '\nTest loss: ' + str(testLoss) + '\nTest accuracy: ' + str(testAccuracy)
@@ -206,11 +200,11 @@ for mass in scaledTestMassPointsList:
     m_train_signal = []
     for trainEvent in X_train_signal:
         m_train_signal.append(trainEvent[-1])
-    X_train_signal_mass = X_train_signal[m_train_signal == scaledTrainMassPointsList[counter]]
+    X_train_signal_mass = X_train_signal[m_train_signal == scaledTestMassPointsList[counter]]
 
     ### Assigning the same mass value to train background events
     for bkg in X_train_bkg:
-          bkg[X_train_bkg.shape[1] - 1] = scaledTrainMassPointsList[counter]
+          bkg[X_train_bkg.shape[1] - 1] = scaledTestMassPointsList[counter]
           
     ### Prediction
     yhat_train_signal_mass, yhat_train_bkg_mass, yhat_test_signal_mass, yhat_test_bkg_mass = PredictionSigBkg(model, X_train_signal_mass, X_train_bkg, X_test_signal_mass, X_test_bkg)
