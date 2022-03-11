5c53293e3cc80f7cab1c8bd2d3321f707f8d4d5a
Alessandra Palazzo <apalazzo@wn-11.le.infn.it>
2022-03-11 09:41:50 +0100

    Faster buildDataset (saving dataframe for each origin and then loading them)

--- buildDataset.py
+++ buildDataset.py
@@ -1,30 +1,38 @@
 from Functions import ReadArgParser, ReadConfig, checkCreateDir, ShufflingData, SelectEvents, CutMasses, DrawVariablesHisto, DrawCorrelationMatrix
 import pandas as pd
 import numpy as np
-import re
-import ast
 import random
 import os.path
 from colorama import init, Fore
 init(autoreset = True)
-import matplotlib
-import matplotlib.pyplot as plt
 
-drawPlots = True
+drawPlots = False
+overwriteDataFrame = True
 
 ### Reading the command line
 tag, jetCollection, analysis, channel, preselectionCuts, signal, background = ReadArgParser()
 
 ### Reading from config file
-inputFiles, rootBranchSubSample, InputFeatures, dfPath, variablesToSave, backgroundsList = ReadConfig(tag, analysis, jetCollection)
+rootBranchSubSample, InputFeatures, dfPath, variablesToSave, backgroundsList = ReadConfig(tag, analysis, jetCollection)
 
-### Creating output directory and logFile
+### Creating output directories and logFile
+tmpOutputDir = dfPath + analysis + '/' + channel
+print(format('First output directory: ' + Fore.GREEN + tmpOutputDir), checkCreateDir(tmpOutputDir))
+tmpFileCommonName = tag + '_' + jetCollection + '_' + analysis + '_' + channel + '_' + preselectionCuts
+
 outputDir = dfPath + analysis + '/' + channel + '/' + signal + '/' + background
-print (format('Output directory: ' + Fore.GREEN + outputDir), checkCreateDir(outputDir))
+print(format('Second output directory: ' + Fore.GREEN + outputDir), checkCreateDir(outputDir))
 fileCommonName = tag + '_' + jetCollection + '_' + analysis + '_' + channel + '_' + preselectionCuts + '_' + signal + '_' + background
 logFileName = outputDir + '/EventsNumberAfterSelection_' + fileCommonName + '.txt'
 logFile = open(logFileName, 'w')
-logFile.write('CxAOD tag: ' + tag + '\nJetCollection: ' + jetCollection + '\nAnalysis: ' + analysis + '\nChannel: ' + channel + '\nPreselection cuts: ' + '\nSignal: ' + signal + '\nBackground: ' + background + '\nInput files path: ' + dfPath + '\nInput files and number of events after selection:\n')
+logFile.write('CxAOD tag: ' + tag + '\nJetCollection: ' + jetCollection + '\nAnalysis: ' + analysis + '\nChannel: ' + channel + '\nPreselection cuts: ' + '\nSignal: ' + signal + '\nBackground: ' + background + '\nInput files path: ' + dfPath)# + '\nInput files and number of events after selection:\n')
+
+### Loading DSID-mass map and storing it into a dictionary
+DictDSID = {}
+DSIDfile = open('DSIDtoMass.txt')
+lines = DSIDfile.readlines()
+for line in lines:
+    DictDSID[int(line.split(':')[0])] = int(line.split(':')[1])
 
 ### Creating the list of the origins selected (signal + background)
 if background == 'all':
@@ -35,42 +43,58 @@ else:
 targetOrigins = originsBkgTest.copy()
 targetOrigins.insert(0, signal)
 
-### Loading DSID-mass map and storing it into a dictionary
-DictDSID = {}
-DSIDfile = open('DSIDtoMass.txt')
-lines = DSIDfile.readlines()
-for line in lines:
-    DictDSID[int(line.split(':')[0])] = int(line.split(':')[1])
-
-### Creating two empty dataframes to fill with signal and background events
+### Creating empty signal and background dataframe 
 dataFrameSignal = []
 dataFrameBkg = []
 
-for file in os.listdir(dfPath):
-    if not file.endswith('.pkl'):
-        continue
-    if file == 'Wjets-mc16d_DF.pkl':
-        continue
-    for target in targetOrigins:
-        if file.startswith(target):
-            print(Fore.GREEN + 'Loading ' + dfPath + file)
-            inputDf = pd.read_pickle(dfPath + file)
-            logFile.write('\n' + file)
-            ### Selecting only variables relevant to the analysis (needed in order to avoid memory issues)
-            inputDf = inputDf[rootBranchSubSample]
-            ### Selecting events according to merged/resolved regime and ggF/VBF channel
-            inputDf = SelectEvents(inputDf, channel, analysis, preselectionCuts)
-            logFile.write(' -> ' + str(inputDf.shape[0]))
-            ### Creating new column in the dataframe with the origin
-            inputDf = inputDf.assign(origin = target)
-            ### Filling signal/background dataframes
+for target in targetOrigins:
+    fileName = tmpOutputDir + '/' + target + '_' + tmpFileCommonName + '.pkl'
+    ### Loading dataframe if found and overwrite flag is false 
+    if not overwriteDataFrame:
+        if os.path.isfile(fileName):
             if target == signal:
-                dataFrameSignal.append(inputDf)
+                print(Fore.GREEN + 'Found signal dataframe: loading ' + fileName)
+                dataFrameSignal = pd.read_pickle(fileName)
             else:
-                dataFrameBkg.append(inputDf)
+                print(Fore.GREEN + 'Found background dataframe: loading ' + fileName)
+                dataFrameBkg.append(pd.read_pickle(fileName))
+                
+    ### Creating dataframe if not found or overwrite flag is true
+    if overwriteDataFrame or not os.path.isfile(fileName):
+        ### Defining local dataframe (we might have found only one among many dataframes)
+        partialDataFrameBkg = []
+        for file in os.listdir(dfPath):
+            ### Loading input file
+            if file.startswith(target) and file.endswith('.pkl'):
+                print(Fore.GREEN + 'Loading ' + dfPath + file)
+                inputDf = pd.read_pickle(dfPath + file)
+                ### Selecting events according to merged/resolved regime and ggF/VBF channel
+                '''
+                print(inputDf.shape)
+                inputDf1 = inputDf.query('Pass_MergHP_GGF_ZZ_Tag_SR == False and Pass_MergHP_GGF_ZZ_Untag_SR == False and Pass_MergHP_GGF_WZ_SR == False and Pass_MergLP_GGF_ZZ_Tag_SR == False and Pass_MergLP_GGF_ZZ_Untag_SR == False and Pass_MergLP_GGF_WZ_SR == False and Pass_MergHP_GGF_ZZ_Tag_ZCR == False and Pass_MergHP_GGF_ZZ_Untag_ZCR == False and Pass_MergHP_GGF_WZ_ZCR == False and Pass_MergLP_GGF_ZZ_Tag_ZCR == False and Pass_MergLP_GGF_ZZ_Untag_ZCR == False and Pass_MergLP_GGF_WZ_ZCR == False and Pass_MergHP_GGF_ZZ_Untag_TCR == False and Pass_MergHP_GGF_ZZ_Tag_TCR == False and Pass_MergHP_GGF_WZ_TCR == False and Pass_MergLP_GGF_ZZ_Untag_TCR == False and Pass_MergLP_GGF_ZZ_Tag_TCR == False and Pass_MergLP_GGF_WZ_TCR == False and Pass_Res_GGF_WZ_SR == False and Pass_Res_GGF_ZZ_Tag_SR == False and Pass_Res_GGF_ZZ_Untag_SR == False and Pass_Res_GGF_WZ_ZCR == False and Pass_Res_GGF_ZZ_Tag_ZCR == False and Pass_Res_GGF_ZZ_Untag_ZCR == False and Pass_Res_GGF_WZ_TCR == False and Pass_Res_GGF_ZZ_Tag_TCR == False and Pass_Res_GGF_ZZ_Untag_TCR == False and Pass_MergHP_VBF_WZ_SR == False and Pass_MergHP_VBF_ZZ_SR == False and Pass_MergLP_VBF_WZ_SR == False and Pass_MergLP_VBF_ZZ_SR == False and Pass_MergHP_VBF_WZ_ZCR == False and Pass_MergHP_VBF_ZZ_ZCR == False and Pass_MergLP_VBF_WZ_ZCR == False and Pass_MergLP_VBF_ZZ_ZCR == False and Pass_MergHP_VBF_WZ_TCR == False and Pass_MergHP_VBF_ZZ_TCR == False and Pass_MergLP_VBF_WZ_TCR == False and Pass_MergLP_VBF_ZZ_TCR == False and Pass_Res_VBF_WZ_SR == False and Pass_Res_VBF_ZZ_SR == False and Pass_Res_VBF_WZ_ZCR == False and Pass_Res_VBF_ZZ_ZCR == False and Pass_Res_VBF_WZ_TCR == False and Pass_Res_VBF_ZZ_TCR == False')
+                print(inputDf1.shape)       
+                exit()
+                '''
+                inputDf = SelectEvents(inputDf, channel, analysis, preselectionCuts)
+                ### Creating new column in the dataframe with the origin
+                inputDf = inputDf.assign(origin = target)
+                ### Filling signal/background dataframes
+                if target == signal:
+                    dataFrameSignal.append(inputDf)
+                else:
+                    partialDataFrameBkg.append(inputDf)
+        ### Concatening and saving signal and background dataframes
+        if target == signal:
+            dataFrameSignal = pd.concat(dataFrameSignal, ignore_index = True)
+            dataFrameSignal.to_pickle(fileName)
+        elif target != signal:
+            partialDataFrameBkg = pd.concat(partialDataFrameBkg, ignore_index = True)
+            partialDataFrameBkg.to_pickle(fileName)
+            ### Appending the local background dataframe to the final one
+            dataFrameBkg.append(partialDataFrameBkg)
+        print(Fore.GREEN + 'Saved ' + fileName)
 
-### Concatening signal and background dataframes
-dataFrameSignal = pd.concat(dataFrameSignal, ignore_index = True)
+### Concatening the global background dataframe
 dataFrameBkg = pd.concat(dataFrameBkg, ignore_index = True)
 
 ### Creating a new isSignal column with values 1 (0) for signal (background) events
@@ -114,12 +138,15 @@ dataFrame = ShufflingData(dataFrame)
 ### Saving number of events for each origin
 for origin in targetOrigins:
     logFile.write('\nNumber of ' + origin + ' events: ' + str(dataFrame[dataFrame['origin'] == origin].shape[0]))
-
-### Saving pkl files
+    print(Fore.BLUE + 'Number of ' + origin + ' events: ' + str(dataFrame[dataFrame['origin'] == origin].shape[0]))
+    
+### Saving the combined dataframe
 outputFileName = '/MixData_' + fileCommonName + '.pkl'
 dataFrame.to_pickle(outputDir + outputFileName)
 print(Fore.GREEN + 'Saved ' + outputDir + outputFileName)
-logFile.write('\nSaved ' + outputDir + outputFileName)
+logFile.write('\nSaved combined (signal and background) dataframe in ' + outputDir + outputFileName)
+
+### Closing the log file
 logFile.close()
 print(Fore.GREEN + 'Saved ' + logFileName)
 
@@ -129,3 +156,4 @@ if drawPlots:
     checkCreateDir(histoOutputDir)
     DrawVariablesHisto(dataFrame, InputFeatures, histoOutputDir, fileCommonName, jetCollection, analysis, channel, signal, background, preselectionCuts)
     DrawCorrelationMatrix(dataFrame, InputFeatures, outputDir, fileCommonName, analysis, channel, signal, background)
+
