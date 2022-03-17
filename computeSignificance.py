1c7b92016738650667810d6f13b472510eb2b52f
Alessandra Palazzo <apalazzo@login.le.infn.it>
2022-03-17 11:37:17 +0100

    New bins for invariant mass (from Rob)

--- computeSignificance.py
+++ computeSignificance.py
@@ -15,6 +15,51 @@ from matplotlib import gridspec
 
 NN = 'PDNN'
 
+def defineBins(regime):
+    #regime = str(regime)
+    print(regime)
+    bins = []
+    if (all(x in regime for x in ['SR', 'Res', 'GGF', 'Tag'])):
+        bins = [300,320,350,380,410,440,480,520,560,600,650,700,750,810,870,940,1010,1090,1170,1260,1360,1460,1650,3000]
+        print(Fore.RED + 'found1')
+    if (all(x in regime for x in ['SR', 'Res', 'GGF', 'Untag'])):
+        bins = [300,320,350,380,410,440,480,520,560,600,650,700,750,810,870,940,1010,1090,1170,1260,1360,1460,1570,1690,1820,1960,2110,3000]
+        print(Fore.RED + 'found2')
+    if (all(x in regime for x in ['SR', 'Res', 'VBF', 'ZZ'])):
+        bins = [300,320,350,380,410,440,470,500,540,580,620,660,710,760,810,870,930,990,1060,1130,1210,1290,1380,1470,1630,1790,3000]
+        print(Fore.RED + 'found3')
+    if (all(x in regime for x in ['SR', 'Merg', 'GGF', 'Tag'])):
+        print(Fore.RED + 'found4')
+        bins = [500,530,570,610,650,690,730,770,810,850,890,930,970,1020,1070,1120,1170,1220,1270,1330,1480,1630,1780,1930,2080,2380,6000]
+    if (all(x in regime for x in ['SR', 'Merg', 'GGF', 'Untag'])):
+        print(Fore.RED + 'found5')
+        bins = [500,530,570,610,650,690,730,770,810,850,890,930,970,1020,1070,1120,1170,1220,1270,1330,1390,1450,1510,1570,1640,1710,1780,1850,1920,2000,2080,2160,2250,2340,2430,2520,2620,2720,2820,3120,3420,4910,6000]
+    if (all(x in regime for x in ['SR', 'Merg', 'VBF', 'ZZ'])):
+        print(Fore.RED + 'found6')
+        bins = [500,530,570,610,650,690,730,770,810,860,910,960,1010,1070,1130,1190,1260,1340,1420,1530,1680,1830,1980,6000]
+    if (all(x in regime for x in ['CR', 'Res', 'GGF', 'ZCR'])):
+        print(Fore.RED + 'found7')
+        bins = [300,320,350,380,410,440,470,500,530,570,610,660,740,900,3000]
+    if (all(x in regime for x in ['CR', 'Res', 'VBF', 'ZCR'])):
+        print(Fore.RED + 'found8')
+        bins = [300, 3000]
+    if (all(x in regime for x in ['CR', 'Merg', 'GGF', 'ZCR'])):
+        print(Fore.RED + 'found9')
+        bins = [500,6000]
+    if (all(x in regime for x in ['CR', 'Merg', 'VBF', 'ZCR'])):
+        print(Fore.RED + 'found10')
+        bins = [500, 6000]
+
+    '''   
+    binsDict = {
+        #SRresGGFelse : [300,320,350,380,410,440,470,500,530,560,600,640,680,720,770,820,870,930,990,1060,1130,1210,1290,1380,1470,1570,1680,1790,2140,3000],
+        #SRresVBFelse : [300,320,350,380,410,440,470,500,540,580,620,660,710,760,810,860,920,980,1040,1110,1180,1250,1330,1410,1640,1870,3000],
+        #SRmergedGGFelse : [500,530,570,610,650,690,730,770,810,850,890,930,970,1010,1050,1100,1150,1200,1250,1300,1350,1410,1470,1530,1590,1650,1720,1790,1860,1930,2000,2080,2160,2240,2330,2510,2690,2870,3320,3770,6000],
+        #SRmergedVBFelse : [500,530,570,610,650,690,730,770,810,850,890,930,970,1010,1050,1090,1130,1170,1210,1250,1300,1350,1410,1470,1630,2000,2370,6000],
+    }
+    '''
+    return bins
+
 ### Reading the command line and extracting analysis and channel
 tag, jetCollection, regime, preselectionCuts, signal, background = ReadArgParser()
 if 'Merg' in regime[0]:
@@ -174,6 +219,7 @@ for regimeToTest in regime:
     significanceDict = {}
     sortedMassList = np.sort(np.array(massesSignalList))
     for feature in ['Scores', 'Invariant mass']:
+        print(feature)
         significanceList = []
         for mass in sortedMassList:
             scaledMass = massesDictionary[mass]
@@ -189,9 +235,20 @@ for regimeToTest in regime:
                 hist_bkg = X_boosted_m_bkg
                 minEdge = min(min(hist_signal), min(hist_bkg))
                 maxEdge = max(max(hist_signal), max(hist_bkg))
-                Bins = np.linspace(minEdge, maxEdge, Nbins)
+                #Bins = np.linspace(minEdge, maxEdge, Nbins)
+                Bins = defineBins(regimeToTest)
             binContentsSignal, binEdgesSignal, _ = plt.hist(hist_signal, weights = signalMCweightsMass, bins = Bins, histtype = 'step', lw = 2, color = 'blue', label = [r'Signal'])#, density = True) ###### density???
             binContentsBkg, binEdgesBkg, _ = plt.hist(hist_bkg, weights = bkgMCweights, bins = Bins, histtype = 'step', lw = 2, color = 'red', label = [r'Background'])#, density = True)
+            if feature == 'Invariant mass': 
+                plt.xlabel('Invariant mass [GeV]')
+                plt.ylabel('Weighted counts')
+                plt.yscale('log')
+                legendText = 'Signal: ' + signal + ' (mass ' + str(mass) + ' GeV)\nBackground: ' + str(background) + '\nRegime: ' + regimeToTest
+                plt.figtext(0.3, 0.7, legendText, wrap = True, horizontalalignment = 'left')
+                plt.legend()
+                pltName = inputOutputDir + '/InvariantMass_' + regimeToTest + '_' + str(mass) + '_' + tag + '_' + jetCollection + '_' + preselectionCuts + '_' + signal + '_' + background + '.png'
+                plt.savefig(pltName)
+                print(Fore.GREEN + 'Saved ' + pltName)
             '''
             if feature == 'Invariant mass':
                 print(binContentsSignal)
@@ -237,14 +294,15 @@ for regimeToTest in regime:
     colorsDict = {'Scores': 'red', 'Invariant mass': 'blue'}
     for key in significanceDict:
         ax1.plot(sortedMassListTeV, significanceDict[key], color = colorsDict[key], marker = 'o', label = key)
-        ax2.plot(sortedMassListTeV, np.divide(np.array(significanceDict[key]), np.array(significanceDict['Scores'])), color = colorsDict[key], marker = 'o')
+        ax2.plot(sortedMassListTeV, np.divide(np.array(significanceDict[key]), np.array(significanceDict['Invariant mass'])), color = colorsDict[key], marker = 'o')
     emptyPlot, = ax1.plot(sortedMassListTeV[0], significanceDict['Scores'][0], color = 'white')#, label = legendText)
-    legend1 = ax1.legend(loc = 'center right')
-    legend2 = ax1.legend([emptyPlot], [legendText], frameon = True, handlelength = 0, handletextpad = 0)
+    legend1 = ax1.legend(loc = 'upper left')
+    legend2 = ax1.legend([emptyPlot], [legendText], frameon = True, handlelength = 0, handletextpad = 0, loc = 'lower right')
     ax1.add_artist(legend1)
     ax1.set(xlabel = 'Mass [TeV]')
     ax1.set(ylabel = 'Significance', yscale = 'log')
     ax2.set(xlabel = 'Mass [TeV]')
+    ax2.set(ylabel = 'Ratio to invariant mass')
     plt.tight_layout()
     fileCommonName = tag + '_' + jetCollection + '_' + preselectionCuts + '_' + signal + '_' + background + '_' + regimeToTest
     pltName = inputOutputDir + '/Significance_' + fileCommonName + '.png'
@@ -256,7 +314,7 @@ for regimeToTest in regime:
 if len(regime) > 1:
     plt.clf()
     legendText = 'jet collection: ' + jetCollection + '\nsignal: ' + signal + '\nbackground: ' + str(background)
-    legendText += '\nRegimes:'
+    legendText += '\nregimes:'
     if (preselectionCuts != 'none'):
         legendText += '\npreselection cuts: ' + preselectionCuts
     for regimeToTest in regime:
@@ -272,14 +330,15 @@ if len(regime) > 1:
     ax1.plot(sortedMassListTeV, combinedSignificanceScores, marker = 'o', color = 'red', label = 'Scores')
     ax1.plot(sortedMassListTeV, combinedSignificanceMass, marker = 'o', color = 'blue', label = 'Invariant mass')
     emptyPlot, = ax1.plot(sortedMassListTeV[0], combinedSignificanceScores[0], color = 'white')#, label = legendText)
-    legend1 = ax1.legend(loc = 'center right')
-    legend2 = ax1.legend([emptyPlot], [legendText], frameon = True, handlelength = 0, handletextpad = 0)
+    legend1 = ax1.legend(loc = 'upper left')
+    legend2 = ax1.legend([emptyPlot], [legendText], frameon = True, handlelength = 0, handletextpad = 0, loc = 'lower right')
     ax1.add_artist(legend1)
-    ax2.plot(sortedMassListTeV, np.divide(np.array(combinedSignificanceScores), np.array(combinedSignificanceScores)), color = 'red', marker = 'o')
-    ax2.plot(sortedMassListTeV, np.divide(np.array(combinedSignificanceScores), np.array(combinedSignificanceMass)), color = 'blue', marker = 'o')
+    ax2.plot(sortedMassListTeV, np.divide(np.array(combinedSignificanceScores), np.array(combinedSignificanceMass)), color = 'red', marker = 'o')
+    ax2.plot(sortedMassListTeV, np.divide(np.array(combinedSignificanceMass), np.array(combinedSignificanceMass)), color = 'blue', marker = 'o')
     ax1.set(xlabel = 'Mass [TeV]')
     ax1.set(ylabel = 'Significance', yscale = 'log')
     ax2.set(xlabel = 'Mass [TeV]')
+    ax2.set(ylabel = 'Ratio to invariant mass')
     pltName = commonDir + 'CombinedSignificance_' + tag + '_' + jetCollection + '_' + analysis + '_' + channel + '_' + preselectionCuts + '_' + signal + '_' + background + regimeString + '.png'
     plt.savefig(pltName)
     print(Fore.GREEN + 'Saved ' + pltName)
