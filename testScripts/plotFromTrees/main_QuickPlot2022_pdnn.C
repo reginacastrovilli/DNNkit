d03d87103bcdeaf8ed612e44581cc9c6f0eada67
Stefania Spagnolo <stefania.spagnolo@cern.ch>
2022-04-09 14:47:34 +0200

    adding main_QuickPlot2022_pdnn.C

--- testScripts/plotFromTrees/main_QuickPlot2022_pdnn.C
+++ testScripts/plotFromTrees/main_QuickPlot2022_pdnn.C
@@ -7,10 +7,10 @@ void main_QuickPlot2022_pdnn()
   // instantiate a chainSet 
   chainSet_33_22_pdnn* cs = new chainSet_33_22_pdnn();
 
-  std::string tag="Run2_";
+  std::string tag="Run2";
   bool useW=true; bool shapeComp=false;
   bool savePlots=true;
-  bool logx=false; bool logy=false;
+  bool logx=false; bool logy=true;
   std::string scoreSignal="";
   std::string userCut="";
   
@@ -35,14 +35,24 @@ void main_QuickPlot2022_pdnn()
   std::string userCutArrayForWZ_ZCR[6]= {"Pass_MergHP_VBF_WZ_ZCR==1", "Pass_MergLP_VBF_WZ_ZCR==1", "Pass_Res_VBF_WZ_ZCR==1", "Pass_MergHP_GGF_WZ_ZCR==1", "Pass_MergLP_GGF_WZ_ZCR==1", "Pass_Res_GGF_WZ_ZCR==1"};
 
 
-  std::string myvar="X_boosted_m";
-  int nBins=200;double xmin=0.;double xmax=3500.; 
+  //std::string myvarArray[4]={"pDNNScore600","pDNNScore800","pDNNScore1000", "pDNNScore3000"};
+  std::string myvarArray[2]={"pDNNScore600","pDNNScore3000"};
+  //int nBins=100;double xmin=0.;double xmax=1.; 
   
+  bool showMass=true;
+  int nBins=100;double xmin=0.;double xmax=4000.; 
+  std::string myvar="";
   
-  scoreSignal="RSG";
+  //  for (unsigned int j=0; j<2; ++j) 
+  //   {
+  //     myvar = myvarArray[j]; 
+  scoreSignal="RSG";	
   for (unsigned int i=0; i<9; ++i)
-    {
+    {	
       userCut=userCutArrayForZZ_SR[i];
+      if (userCut.substr(0,9)=="Pass_Merg" && showMass) myvar="X_boosted_m";
+      else if (showMass) myvar="X_resolved_ZZ_m";
+
       makeMyQuickPlot2022_pdnn(myvar, nBins, xmin, xmax, logx, logy, cs, userCut, tag, useW, shapeComp, scoreSignal, savePlots);
       userCut=userCutArrayForZZ_ZCR[i];
       makeMyQuickPlot2022_pdnn(myvar, nBins, xmin, xmax, logx, logy, cs, userCut, tag, useW, shapeComp, scoreSignal, savePlots);
@@ -51,6 +61,8 @@ void main_QuickPlot2022_pdnn()
   for (unsigned int i=0; i<9; ++i)
     {
       userCut=userCutArrayForZZ_SR[i];
+      if (userCut.substr(0,9)=="Pass_Merg" && showMass) myvar="X_boosted_m";
+      else if (showMass) myvar="X_resolved_ZZ_m";
       makeMyQuickPlot2022_pdnn(myvar, nBins, xmin, xmax, logx, logy, cs, userCut, tag, useW, shapeComp, scoreSignal, savePlots);
       userCut=userCutArrayForZZ_ZCR[i];
       makeMyQuickPlot2022_pdnn(myvar, nBins, xmin, xmax, logx, logy, cs, userCut, tag, useW, shapeComp, scoreSignal, savePlots);
@@ -60,11 +72,14 @@ void main_QuickPlot2022_pdnn()
   for (unsigned int i=0; i<6; ++i)
     {
       userCut=userCutArrayForWZ_SR[i];
+      if (userCut.substr(0,9)=="Pass_Merg" && showMass) myvar="X_boosted_m";
+      else if (showMass) myvar="X_resolved_WZ_m";
       makeMyQuickPlot2022_pdnn(myvar, nBins, xmin, xmax, logx, logy, cs, userCut, tag, useW, shapeComp, scoreSignal, savePlots);
       userCut=userCutArrayForWZ_ZCR[i];
       makeMyQuickPlot2022_pdnn(myvar, nBins, xmin, xmax, logx, logy, cs, userCut, tag, useW, shapeComp, scoreSignal, savePlots);
     }
 
-
+  //    }
+  
 
 }
